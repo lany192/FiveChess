b35511e2340f877285843e6c696e329704026987
Lany <lany192@qq.com>
2017-05-19 09:30:06 +0800

    update

---
+++ app/src/main/java/com/lany/fivechess/game/Point.kt
@@ -0,0 +1,18 @@
+package com.lany.fivechess.game
+
+/**
+ * 坐标类
+ */
+class Point {
+    var x: Int = 0
+    var y: Int = 0
+
+    constructor() {
+
+    }
+
+    constructor(x: Int, y: Int) {
+        this.x = x
+        this.y = y
+    }
+}
